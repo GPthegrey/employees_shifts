764cd7fa9c37209d0d11e168701a4ce34dad2b2f
Guido Palmili <guidopalmili@gmail.com>
2024-04-11 11:49:53 -0300

    created the bank holiday controller and create method

--- app/controllers/bank_holiday_controller.rb
+++ app/controllers/bank_holiday_controller.rb
@@ -1,2 +1,16 @@
 class BankHolidayController < ApplicationController
+  def create
+    @bank_holiday = BankHoliday.new(bank_holiday_params)
+    if @bank_holiday.save
+      redirect_to shifts_path
+    else
+      render 'shifts/index'
+    end
+  end
+
+  private
+
+  def bank_holiday_params
+    params.require(:bank_holiday).permit(:date, :name)
+  end
 end
